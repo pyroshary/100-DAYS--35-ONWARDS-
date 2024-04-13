ccc713d1531c2e0542b139b15268a9ee06d04808
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-13 23:28:32 +0500

    DAY 38

--- DAY 38/Q 112/index.js
+++ DAY 38/Q 112/index.js
@@ -6,3 +6,13 @@ countries.set("JAPAN", "TOKYO");
 countries.set("FRANCE", "PARIS");
 countries.set("PAKISTAN", "KARACHI");
 console.log(countries);
+//answerof q 113
+function capitalofCanada(countries) {
+    if (countries.has("PAKISTAN")) {
+        console.log(`The capital of PAkistan is ${countries.get("PAKISTAN")}`);
+    }
+    else {
+        console.log("pakistan is not in your map");
+    }
+}
+capitalofCanada(countries);
--- DAY 38/Q 112/index.ts
+++ DAY 38/Q 112/index.ts
@@ -9,3 +9,17 @@ countries.set("FRANCE","PARIS")
 countries.set("PAKISTAN","KARACHI")
 
 console.log(countries);
+
+
+//answerof q 113
+
+function capitalofCanada(countries:Map<string,string>): void{
+    if(countries.has("PAKISTAN")){
+        console.log(`The capital of PAkistan is ${countries.get("PAKISTAN")}`);
+    }
+    else {
+        console.log("pakistan is not in your map")
+    }
+}
+
+capitalofCanada(countries);
