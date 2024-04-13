085749eaba59ff0dfafac94f55161a9c57c780d8
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-13 05:51:51 +0500

    DAY 37

---
+++ DAY 37/Q 109/index.js
@@ -0,0 +1,5 @@
+//print good morning if the time is before 12 Am
+var currentTime = new Date();
+if (currentTime.getHours() < 12) {
+    console.log("Good Morning");
+}
---
+++ DAY 37/Q 109/index.ts
@@ -0,0 +1,7 @@
+//print good morning if the time is before 12 Am
+
+const currentTime = new Date();
+if (currentTime.getHours() <12)
+    {
+    console.log("Good Morning")
+}
