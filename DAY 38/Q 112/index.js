25628b5f63116cfd989e709802fee37e82fc6152
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-13 23:17:46 +0500

    DAY 38

---
+++ DAY 38/Q 112/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const countries = new Map();
+countries.set("USA", "Washington, D.c");
+countries.set("JAPAN", "TOKYO");
+countries.set("FRANCE", "PARIS");
+countries.set("PAKISTAN", "KARACHI");
+console.log(countries);
---
+++ DAY 38/Q 112/index.ts
@@ -0,0 +1,11 @@
+const countries = new  Map<string , string>()
+
+countries.set("USA","Washington, D.c")
+
+countries.set("JAPAN","TOKYO")
+
+countries.set("FRANCE","PARIS")
+
+countries.set("PAKISTAN","KARACHI")
+
+console.log(countries);
