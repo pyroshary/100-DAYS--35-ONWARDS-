ec72214280d2e2fcb71133df7cd20fc3f866a631
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-14 00:56:06 +0500

    DAY 40

---
+++ DAY 40/Q 120/index.js
@@ -0,0 +1,10 @@
+var favtMovie = [
+    "TERMINTOR",
+    "IRON MEN",
+    "SPIDER MEN",
+    "SUPER MEN"
+];
+for (var _i = 0, favtMovie_1 = favtMovie; _i < favtMovie_1.length; _i++) {
+    var movie = favtMovie_1[_i];
+    console.log(movie);
+}
---
+++ DAY 40/Q 120/index.ts
@@ -0,0 +1,11 @@
+const favtMovie:string[] = [
+    "TERMINTOR",
+    "IRON MEN",
+    "SPIDER MEN",
+    "SUPER MEN"
+];
+
+
+for(const movie of favtMovie){
+    console.log(movie);
+}
