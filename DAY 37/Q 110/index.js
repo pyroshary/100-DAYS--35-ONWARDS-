d2ef63353f45edc811ea0a2c7679257d68555423
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-13 06:01:42 +0500

    new commit

---
+++ DAY 37/Q 110/index.js
@@ -0,0 +1,20 @@
+function assignGrade(score) {
+    if (score >= 90) {
+        return "A";
+    }
+    else if (score >= 80) {
+        return "B";
+    }
+    else if (score >= 70) {
+        return "C";
+    }
+    else if (score >= 60) {
+        return "D";
+    }
+    else {
+        return "FAIL";
+    }
+}
+console.log(assignGrade(80));
+console.log(assignGrade(70));
+console.log(assignGrade(40));
