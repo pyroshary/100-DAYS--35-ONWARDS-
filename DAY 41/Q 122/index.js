7cd64afbaf88baf3f80dd9fb97b102cdf1f8dcbf
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-14 22:20:02 +0500

    DAY 41

---
+++ DAY 41/Q 122/index.js
@@ -0,0 +1,8 @@
+var counter = 10;
+while (counter > 0) {
+    if (counter === 2) {
+        break;
+    }
+    console.log(counter);
+    counter--;
+}
---
+++ DAY 41/Q 122/index.ts
@@ -0,0 +1,8 @@
+ let counter: number =10;
+ while (counter>0){
+    if (counter ===2){
+        break;
+    }
+    console.log(counter);
+    counter--;
+ }
