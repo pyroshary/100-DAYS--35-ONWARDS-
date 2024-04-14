4ba6d6cbf524752c1f505e5e2ca1fdba677d5484
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-14 23:53:37 +0500

    DAY 43

---
+++ DAY 43/Q 128/index.js
@@ -0,0 +1,8 @@
+var multiplyParameters = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (total, number) { return total * number; }, 1);
+};
+console.log(multiplyParameters(2, 3, 4, 5));
