bbb873f057337f60a0186a0b6b223e42737c4846
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-14 23:14:09 +0500

    DAY 42

---
+++ DAY 42/Q 126/index.ts
@@ -0,0 +1,13 @@
+ const myObject = {
+    property: "Value",
+    outerMethod: function(){
+        console.log(this.property);
+
+        const innerMethod =() =>{
+            console.log(this.property);
+        }
+        innerMethod();
+    },
+ };
+
+ myObject.outerMethod();
