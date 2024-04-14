d1c85816dc07f3ef40d9204205410b855c804455
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-14 23:40:57 +0500

    DAY 43

---
+++ DAY 43/Q 127/index.js
@@ -0,0 +1,6 @@
+var traditionalFunction = function (a, b) {
+    return a + b;
+};
+var arrowtoFunction = function (a, b) { return a + b; };
+console.log(traditionalFunction(5, 5));
+console.log(arrowtoFunction(5, 5));
---
+++ DAY 43/Q 127/index.ts
@@ -0,0 +1,8 @@
+const traditionalFunction = function (a,b){
+    return a + b;
+};
+const arrowtoFunction =(a,b) => a + b;
+
+console.log(traditionalFunction(5,5));
+
+console.log(arrowtoFunction(5,5));
