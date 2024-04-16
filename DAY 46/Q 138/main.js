5c3213691b9a222fc4315d64da67bde0bde4d9ee
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-16 12:58:33 +0500

    DAY 46

---
+++ DAY 46/Q 138/main.js
@@ -0,0 +1,3 @@
+console.log("Before breakpoint");
+console.log("This line has a break point set on it ine the browse's developer tool");
+console.log("After breakpoint");
---
+++ DAY 46/Q 138/main.ts
@@ -0,0 +1,6 @@
+console.log("Before breakpoint");
+
+
+console.log("This line has a break point set on it ine the browse's developer tool");
+
+console.log("After breakpoint")
