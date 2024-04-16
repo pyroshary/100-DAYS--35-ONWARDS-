0b4ce246477699ff8e3fdfb63ee7ceef13dc9db2
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-16 12:15:23 +0500

    DAY 45

---
+++ DAY 45/Q 134/main.js
@@ -0,0 +1,3 @@
+var jsonString = '{"name:"Shary","age":23,"city":"Karachi"}';
+var person = JSON.parse(jsonString);
+console.log(person);
---
+++ DAY 45/Q 134/main.ts
@@ -0,0 +1,6 @@
+const jsonString = '{"name:"Shary","age":23,"city":"Karachi"}'
+
+const person = JSON.parse(jsonString);
+
+console.log(person);
+
