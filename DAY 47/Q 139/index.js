6ed8e9bb389d43da010e709b6122bc54ec166899
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-20 12:34:41 +0500

    DAY 47

---
+++ DAY 47/Q 139/index.js
@@ -0,0 +1,7 @@
+var count = 5;
+if (count > 0) {
+    console.log("COunt is greater than 0");
+}
+function add(a, b) {
+    return a + b;
+}
---
+++ DAY 47/Q 139/index.ts
@@ -0,0 +1,8 @@
+let  count = 5;
+ if (count > 0){
+    console.log("COunt is greater than 0")
+ }
+
+ function add(a,b) {
+    return a + b
+ }
